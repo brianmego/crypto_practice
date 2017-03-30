632a2dad78fa4d43325ef772f5a556e2c781efdd
Brian Mego <brian.mego@q2ebanking.com>
2017-03-29 22:03:33 -0500

    Implements repeating single key xor

--- set_one.py
+++ set_one.py
@@ -1,3 +1,7 @@
+"""
+https://cryptopals.com/sets/1
+"""
+
 import codecs
 import base64
 
@@ -6,8 +10,26 @@ CHAR_FREQ_MAP = {
     'english': 'ETAOIN SHRDLU'
 }
 
-def str_to_hex_bytes(hex_str: str):
+
+def _hex_str_to_hex_bytes(hex_str: str):
     return codecs.decode(hex_str, 'hex')
+
+
+def _hex_bytes_to_hex_str(hex_bytes: str):
+    return codecs.encode(hex_bytes, 'hex').decode('utf8')
+
+
+def _xor_two_hex_byte_strings(hex_bytes_1, hex_bytes_2):
+    return bytes(x ^ y for x, y in zip(hex_bytes_1, hex_bytes_2))
+
+
+def _score_char_freq(string_to_score: str, language: str='english'):
+    score = 0
+    language_char_freq = CHAR_FREQ_MAP[language] + CHAR_FREQ_MAP[language].lower()
+    for char in string_to_score:
+        if chr(char) in language_char_freq:
+            score += 1
+    return score
 
 
 def hex_to_base64(hex_str: str):
@@ -16,33 +38,30 @@ def hex_to_base64(hex_str: str):
     return b64_bytes.decode()
 
 
-def xor_two_hex_byte_strings(hex_bytes_1, hex_bytes_2):
-    return bytes(x ^ y for x, y in zip(hex_bytes_1, hex_bytes_2))
-
-
 def xor_two_strings(hex_str1: str, hex_str2: str):
-    hex_bytes_1 = str_to_hex_bytes(hex_str1)
-    hex_bytes_2 = str_to_hex_bytes(hex_str2)
-    xored_bytes = xor_two_hex_byte_strings(hex_bytes_1, hex_bytes_2)
-    hex_str = codecs.encode(xored_bytes, 'hex').decode('utf8')
+    hex_bytes_1 = _hex_str_to_hex_bytes(hex_str1)
+    hex_bytes_2 = _hex_str_to_hex_bytes(hex_str2)
+    xored_bytes = _xor_two_hex_byte_strings(hex_bytes_1, hex_bytes_2)
+    hex_str = _hex_bytes_to_hex_str(xored_bytes)
     return hex_str
 
 
 def decrypt_xor_cipher(hex_str: str):
-    hex_bytes_1 = str_to_hex_bytes(hex_str)
+    hex_bytes_1 = _hex_str_to_hex_bytes(hex_str)
     decrypted_scores = []
     for char in range(256):
         char = chr(char)
         repeated_cypher = (char * len(hex_bytes_1)).encode('utf8')
-        decrypted_bytes = xor_two_hex_byte_strings(
+        decrypted_bytes = _xor_two_hex_byte_strings(
             hex_bytes_1,
             repeated_cypher
         )
         decrypted_scores.append(
-            (char, score_char_freq(decrypted_bytes), decrypted_bytes, hex_str)
+            (char, _score_char_freq(decrypted_bytes), decrypted_bytes, hex_str)
         )
     sorted_scores = sorted(decrypted_scores, key=lambda x: x[1])
     return sorted_scores[-1]
+
 
 def detect_single_character_xor(list_of_hex_str: list):
     decrypted_scores = []
@@ -51,10 +70,12 @@ def detect_single_character_xor(list_of_hex_str: list):
     sorted_scores = sorted(decrypted_scores, key=lambda x: x[1])
     return sorted_scores[-1]
 
-def score_char_freq(string_to_score: str, language: str='english'):
-    score = 0
-    language_char_freq = CHAR_FREQ_MAP[language] + CHAR_FREQ_MAP[language].lower()
-    for char in string_to_score:
-        if chr(char) in language_char_freq:
-            score += 1
-    return score
+
+def encrypt_repeating_key_xor(plaintext: str, key: str):
+    plaintext_bytes = plaintext.encode('utf8')
+    key_bytes = key.encode('utf8')
+    encrypted_bytes = []
+    for i in range(len(plaintext_bytes)):
+        xored_byte = key_bytes[i % len(key_bytes)] ^ plaintext_bytes[i]
+        encrypted_bytes.append(xored_byte)
+    return codecs.encode(bytes(encrypted_bytes), 'hex')
--- test/test_one.py
+++ test/test_one.py
@@ -27,3 +27,9 @@ def test_detect_byte_xor_cipher():
     actual = set_one.detect_single_character_xor(inputs)[3]
     expected = '7b5a4215415d544115415d5015455447414c155c46155f4058455c5b523f'
     tools.eq_(actual, expected)
+
+def test_encrypt_with_repeating_key_xor():
+    inp = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+    actual = set_one.encrypt_repeating_key_xor(inp, 'ICE')
+    expected = b'0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
+    tools.eq_(actual, expected, '\n{}\n{}'.format(actual, expected))
